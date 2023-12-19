20cb30480922cd68b551d579bc7d9542a0a6b191
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-12-19 20:11:30 +0200

    CODE: aded angular form to register

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
+import { ThrowStmt } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from 'app/services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages'
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  name: String
+  username: String
+  email: String
+  password: String
+  constructor(private validateService: ValidateService , private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+    //required fields
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessage.show("Please fill all fields", {cssClass: 'alert-danger', timeout: 3000})
+      return false
+    }
+    
+
+    // validate email
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessage.show("Please user correct email", {cssClass: 'alert-danger', timeout: 3000})
+      return false
+    }
   }
 
 }
