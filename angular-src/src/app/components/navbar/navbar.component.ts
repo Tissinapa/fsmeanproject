4ea4137be25f0dfba7ab8c08a5881e0afbea81b2
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2024-01-11 17:00:26 +0200

    CODE: added guard so user needs to login to gain access to profile

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -26,7 +26,7 @@ export class NavbarComponent implements OnInit {
     this.flashMessage.show("Logged out", {
       cssClass: "alert-success", timeout: 3000
     })
-    this.router.navigate(['/login'])
+    this.router.navigate(['/'])
     return false
 
   }
