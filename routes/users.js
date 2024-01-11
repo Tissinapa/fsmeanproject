8768cb833641211c258f82729401b714b129873b
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2024-01-11 14:41:29 +0200

    CODE: added login and logout

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,7 @@ router.post('/authenticate',(req,res,next)=>{
     User.getUserByUsername(username, (err, user)  =>{
         if(err) throw err
         if(!user){
-            return res.json({success: false, mes: "User not found"})
+            return res.json({success: false, msg: "User not found"})
         }
         User.comparePassword(password, user.password, (err,isMatch)=>{
             if(err) throw err
